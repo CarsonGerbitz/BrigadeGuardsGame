749b70c490b0fa18b9e40083c61c0eed738436ae
WakeArisato <52734@sunprairieschools.org>
2016-11-21 16:06:18 -0600

    Guardians V.122116
    Game Shyt

--- Guardians V.112116.py
+++ Guardians V.112116.py
@@ -1,52 +1,44 @@
 def HG():
     '''Starts the Guardians TBB Game'''
     Char_Hth = 0
-    Char_Dmg = 0
+    Char_Agl = 0
     Char_Def = 0
-    Char_Dge = 0
     Hstl_Hth = 0
-    Hstl_Dmg = 0
     Hstl_Def = 0
-    Hstl_Dge = 0
+    Hstl_Agl = 0
     Btle_Trn = 0
     import random
     random = random.randint(1,3)
     def Char1():
         '''Wake'''
         Char_Hth = 50
-        Char_Dmg = 100
         Char_Def = 25
-        Char_Dge = 75
+        Char_Agl = 75
     def Char2():
         '''Dave'''
-        Char_Hth = 100
-        Char_Dmg = 50
-        Char_Def = 75
-        Char_Dge = 25
+        Char_Hth = 50
+        Char_Def = 25
+        Char_Agl = 75
     def Char3():
         '''J'''
-        Char_Hth = 75
-        Char_Dmg = 75
-        Char_Def = 75
-        Char_Dge = 75
+        Char_Hth = 50
+        Char_Def = 25
+        Char_Agl = 75
     def Hstl1():
         '''Amygdala'''
         Hstl_Hth = 100
-        Hstl_Dmg = 75
         Hstl_Def = 75
-        Hstl_Dge = 50
+        Hstl_Agl = 25
     def Hstl2():
         '''Paarl'''
-        Hstl_Hth = 75
-        Hstl_Dmg = 75
+        Hstl_Hth = 50
         Hstl_Def = 100
-        Hstl_Dge = 50
+        Hstl_Agl = 50
     def Hstl3():
         '''Nightmare'''
         Hstl_Hth = 75
-        Hstl_Dmg = 100
-        Hstl_Def = 100
-        Hstl_Dge = 25
+        Hstl_Def = 50
+        Hstl_Agl = 75
     if random == 1:
         Hstl1()
     if random == 2:
