d0fba201c30dfe5b5b1ce439bcbb51115af918f7
WakeArisato <52734@sunprairieschools.org>
2016-11-22 11:34:20 -0600

    Check Update
    Added Ether to the check and added more dialog.

--- Check.py
+++ Check.py
@@ -1,4 +1,6 @@
 def Check():
-    from HstlSetup import Hstl_name, Hstl_hp_current, Hstl_hp_max
-    print(Hstl_name)
-    print(Hstl_hp_current + "/" + Hstl_hp_max + "HP")
+    from HstlSetup import Hstl_name, Hstl_hp_current, Hstl_hp_max, Hstl_etr_current, Hstl_etr_max
+    print("It appears that ",Hstl_name," is their Dark Beast!")
+    print("They have ",Hstl_hp_current + "/" + Hstl_hp_max + "HP.")
+    print("They have ",Hstl_etr_current + "/" + Hstl_etr_max + "Ether.")
+    
