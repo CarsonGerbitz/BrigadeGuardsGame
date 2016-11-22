7bc4292243ad15c14bc7c53d05053541c45eefb3
WakeArisato <52734@sunprairieschools.org>
2016-11-21 19:38:18 -0600

    Etr_Update
    Added Etr

--- CharSetup.py
+++ CharSetup.py
@@ -3,24 +3,30 @@ Char = input("Wake, Dave, or J? ")
 if Char.lower() == 'wake':
     Char_name = 'Wake'
     Char_hp_current = 50
+    Char_etr_max = 50
+    Char_etr_current = 50
     Char_hp_max = 50
     Char_def = 25
     Char_agl = 75
     Char_alive = 1
-    Turn = 1
+    Turn = 0
 if Char.lower() == 'dave':
     Char_name = 'Dave'
     Char_hp_current = 50
     Char_hp_max = 50
+    Char_etr_max = 50
+    Char_etr_current = 50
     Char_def = 75
     Char_agl = 25
     Char_alive = 1
-    Turn = 1
+    Turn = 0
 if Char.lower() == 'j':
     Char_name = 'J'
     Char_hp_current = 50
     Char_hp_max = 50
+    Char_etr_max = 50
+    Char_etr_current = 50
     Char_def = 50
     Char_agl = 50
     Char_alive = 1
-    Turn = 1
+    Turn = 0
