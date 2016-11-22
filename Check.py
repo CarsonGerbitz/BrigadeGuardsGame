a9301c0fe5970ef362a750d442b58ee9e0b02f53
CarsonGerbitz <carson52971@gmail.com>
2016-11-22 11:20:32 -0600

    Changed check

--- Check.py
+++ Check.py
@@ -1,12 +1,4 @@
 def Check():
-    from CharSetup import Char_name, Char_hp_current, Char_hp_max
     from HstlSetup import Hstl_name, Hstl_hp_current, Hstl_hp_max
-    print("Who shall we check?")
-    action = input(Char_name + " or " + Hstl_name + "?")
-    if (action).lowercase == Char_name or (action).lowercase == 'me':
-        print(Char_hp_current + "/" + Char_hp_max)
-    if (action).lowercase == Hstl_name or (action).lowercase == 'them':
-        print(Hstl_hp_current + "/" + Hstl_hp_max)
-    else:
-        print("You can CHECK " + Char_name + " or " + Hstl_name)
-        Check()
+    print(Hstl_name)
+    print(Hstl_hp_current + "/" + Hstl_hp_max + "HP")
