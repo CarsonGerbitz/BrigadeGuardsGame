8473cdd8e2ed7555c5351b67dcc59608d9c6ae7f
WakeArisato <52734@sunprairieschools.org>
2016-11-22 18:18:59 -0600

    Check Update
    also i removed the Pychaches

--- Check.py
+++ Check.py
@@ -1,6 +1,6 @@
 def Check():
     from HstlSetup import Hstl_name, Hstl_hp_current, Hstl_hp_max, Hstl_etr_current, Hstl_etr_max
     print("It appears that ",Hstl_name," is their Dark Beast!")
-    print("They have " + str(Hstl_hp_current) + "/" + str(Hstl_hp_max) + "HP.")
-    print("They have " + str(Hstl_etr_current) + "/" + str(Hstl_etr_max) + "Ether.")
+    print("They have " + str(Hstl_hp_current) + "/" + str(Hstl_hp_max) + " HP.")
+    print("They have " + str(Hstl_etr_current) + "/" + str(Hstl_etr_max) + " Ether.")
     
