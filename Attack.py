672379f76132a779896c544bf3d3fc7b69916b2b
CarsonGerbitz <carson52971@gmail.com>
2016-11-22 19:34:32 -0600

    Attack and Damage
    from attack: '''Light heal heals for 15.
    Blood Rush makes it where the less health you have, the more damage you
    do and the more Agl you have.
    Mutilation is a next attack Large Damage boost for a bit of your
    health'''
    
    '''Heavy Heal heals for 25.
    Bolt Strike is a Large ammount of Bolt damage.
    Buckle Armor increases defence for 3 rounds.'''
    
    '''Mid Heal heals for 20.
    Barrel Induction does a large ammount of fire damage.
    Reinforced Pellits increases your damage for 5 rounds.'''

--- Attack.py
+++ Attack.py
@@ -4,33 +4,73 @@ def Attack():
     if action.lower() == 'melee':
         from CharSetup import Char_name
         if Char_name.lower() == 'wake':
-            print("You use the Scythe and you can do either a 'Light Attack' or a 'Precise Attack'")
-        elif Char_name.lower() == 'finrear':
-            print("You use the Great Hammer and you can do either a 'Heavy Attack' or a ' Medium Attack'")
-        elif Char_name.lower() == 'chrono':
-            print("You use Evelyn(musket) and you can either shoot 'Rapid Fire' or 'Precise'")
-        elif Char_name.lower() == 'wake' or Char_name.lower == 'finrear':
+            print("You use the Scythe and you can do either a 'Light Attack' or a 'Heavy Attack'")
             Char_wep_current = input("How do you want to swing? ")
             Char_att_current = 1
+            if Char_wep_current.lower() == 'la' or Char_wep_current.lower() == 'light attack':
+                Char_wep_current = 'light attack'
+            elif Char_wep_current.lower() == 'ha' or Char_wep_current.lower() == 'heavy attack':
+                Char_wep_current = 'heavy attack'
+            else:
+                print("Please try again!")
+                Attack()
+        elif Char_name.lower() == 'finrear':
+            print("You use the Great Hammer and you can do either a 'Heavy Attack' or a ' Medium Attack'")
+            Char_wep_current = input("How do you want to swing? ")
+            Char_att_current = 1
+            if Char wep_current.lower() == 'ma' or Char wep_current.lower() == 'medium attack':
+                Char_wep_current = 'medium attack'
+            elif Char_wep_current.lower() == 'ha' or Char_wep_current.lower() == 'heavy attack':
+                Char_wep_current = 'heavy attack'
+            else:
+                print("Please try again!")
+                Attack()
         elif Char_name.lower() == 'chrono':
+            print("You use Evelyn(musket) and you can either shoot 'Rapid Fire' or 'Precise'")
             Char_wep_current = input("How do you want to shoot? ")
             Char_att_current = 1
+            if Char_wep_current.lower() == 'rf' or Char_wep_current.lower() == 'rapid fire':
+                Char_wep_current = 'rapid fire'
+            elif Char_wep_current.lower() == 'p' or Char_wep_current.lower() == 'precise':
+                Char_wep_current = 'precise':
+            else:
+                print("Please try again!")
+                Attack()
     if action.lower() == 'Ether':
         from CharSetup import Char_name
         if Char_name.lower() == 'wake':
             print("Your spells are 'Light Heal', 'Blood Rush', and 'Mutilation'")
-            '''Light heal heals for 15.
-    Blood Rush makes it where the less health you have, the more damage you do and the more Agl you have.
-    Mutilation is a next attack Large Damage boost for a bit of your health'''
+            Char_spell_current = input("What spell will you use? ")
+            Char_att_current == 2
+            if Char_spell_current.lower() == 'lh' or Char_spell_current.lower() == 'light heal':
+                Char_spell_current = 'light heal'
+            elif Char_spell_current.lower() == 'br' or Char_spell_current.lower() == 'blood rush':
+                Char_spell_current = 'blood rush'
+            elif Char_spell_current.lower() == 'm' or Char_spell_current.lower() == 'mutilation':
+                Char_spell_current = 'mutilation'
+            else:
+                print("Please try again!")
         elif Char_name.lower() == 'finrear':
             print("Your spells are 'Heavy Heal', 'Bolt Strike', and 'Buckle Armor'")
-            '''Heavy Heal heals for 25.
-    Bolt Strike is a Large ammount of Bolt damage.
-    Buckle Armor increases defence for 3 rounds.'''
+            Char_spell_current = input("What spell will you use? ")
+            Char_att_current == 2
+            if Char_spell_current.lower() == 'hh' or Char_spell_current.lower() == 'heavy heal':
+                Char_spell_current = 'heavy heal'
+            elif Char_spell_current.lower() == 'ba' or Char_spell_current.lower() == 'buckle armor':
+                Char_spell_current = 'blood rush'
+            elif Char_spell_current.lower() == 'bs' or Char_spell_current.lower() == 'bolt strike':
+                Char_spell_current = 'mutilation'
+            else:
+                print("Please try again!")
         elif Char_name.lower() == 'chrono':
             print("Your spells are 'Mid Heal', 'Barrel Induction', and 'Reinforced Pellets'")
-            '''Mid Heal heals for 20.
-    Barrel Induction does a large ammount of fire damage.
-    Reinforced Pellits increases your damage for 5 rounds.'''
-        Char_spell_current = input("What spell will you use? ")
-        Char_att_current == 2
+            Char_spell_current = input("What spell will you use? ")
+            Char_att_current == 2
+            if Char_spell_current.lower() == 'mh' or Char_spell_current.lower() == 'mid heal':
+                Char_spell_current = 'mid heal'
+            elif Char_spell_current.lower() == 'bi' or Char_spell_current.lower() == 'barrel induction':
+                Char_spell_current = 'barrel induction'
+            elif Char_spell_current.lower() == 'rp' or Char_spell_current.lower() == 'reinforced pellets':
+                Char_spell_current = 'reinforced pellets'
+            else:
+                print("Please try again!")
--- Damage.py
+++ Damage.py
@@ -1,21 +1,63 @@
 def Char():
     from Attack import Char_att_current
     if Char_att_current == 1:
+        import random
         from Attack import Char_wep_current
         from HstlSetup import Hstl_hp_current
-        from CharSetup import Char_name, Char_hp_current
+        from CharSetup import Char_name, Char_hp_current, Char_hp_max
         if Char_name.lower() == 'wake' and Char_wep_current.lower() == 'light attack':
-            Char_dmg_current = 50
-        if Char_name.lower() == 'wake' and Char_wep_current.lower() == 'precise attack':
-            Char_dmg_current = 50
-        if Char_name.lower() == 'finrear' and Char_wep_current.lower() == 'heavy attack':
-            Char_dmg_current = 50
-        if Char_name.lower() == 'finrear' and Char_wep_current.lower() == 'medium attack':
-            Char_dmg_current = 50
-        if Char_name.lower() == 'chrono' and Char_wep_current.lower() == 'rapid fire':
-            Char_dmg_current = 50
-        if Char_name.lower() == 'chrono' and Char_wep_current.lower() == 'precise':
-            Char_dmg_current = 50
+            Hit_roll = random.randint(1,100)
+            Hit_chance = 90
+            if Hit_roll <= Hit_chance:
+                Char_dmg_current = 10
+                print("You hit with " + str(Char_wep_current.lower()) + " for " + str(Char_dmg_current) + "!")
+            else:
+                print("You missed.")
+        elif Char_name.lower() == 'wake' and Char_wep_current.lower() == 'heavy attack':
+            Hit_roll = random.randint(1,100)
+            Hit_chance = 60
+            if Hit_roll <= Hit_chance:
+                Char_dmg_current = 20
+                print("You hit with " + str(Char_wep_current.lower()) + " for " + str(Char_dmg_current) + "!")
+            else:
+                print("You missed.")
+        elif Char_name.lower() == 'finrear' and Char_wep_current.lower() == 'heavy attack':
+            Hit_roll = random.randint(1,100)
+            Hit_chance = 60
+            if Hit_roll <= Hit_chance:
+                Char_dmg_current = 20
+                print("You hit with " + str(Char_wep_current.lower()) + " for " + str(Char_dmg_current) + "!")
+            else:
+                print("You missed.")
+        elif Char_name.lower() == 'finrear' and Char_wep_current.lower() == 'medium attack':
+            Hit_roll = random.randint(1,100)
+            Hit_chance = 75
+            if Hit_roll <= Hit_chance:
+                Char_dmg_current = 15
+                print("You hit with " + str(Char_wep_current.lower()) + " for " + str(Char_dmg_current) + "!")
+            else:
+                print("You missed.")
+        elif Char_name.lower() == 'chrono' and Char_wep_current.lower() == 'rapid fire':
+            Hit_roll = random.randint(1,100)
+            Hit_chance = 70
+            if Hit_roll <= Hit_chance:
+                Char_dmg_current = 10
+                Hit_roll = random.randint(1,100)
+                if Hit_roll <= Hit_chance:
+                    Char_dmg_current = Char_dmg_current + 10
+                    print("You hit with " + str(Char_wep_current.lower()) + " for " + str(Char_dmg_current) + "!")
+                else:
+                    print("You hit with " + str(Char_wep_current.lower()) + " for " + str(Char_dmg_current) + "!")
+            else:
+                print("You missed.")
+        elif Char_name.lower() == 'chrono' and Char_wep_current.lower() == 'precise':
+            Hit_roll = random.randint(1,100)
+            Hit_chance = 90
+            if Hit_roll <= Hit_chance:
+                Char_dmg_current = 10
+                print("You hit with " + str(Char_wep_current.lower()) + " for " + str(Char_dmg_current) + "!")
+            else:
+                print("You missed.")
     if Char_att_current == 2:
         from Attack import Char_spell_current
         if Char_name.lower() == 'wake' and Char_spell_current.lower() == 'light heal':
@@ -23,13 +65,21 @@ def Char():
                 Char_hp_current = Char_hp_max
             else:
                 Char_hp_current = Char_hp_current + 15
-        if Char_name.lower() == 'wake' and Char_spell_current.lower() == 'blood rush':
-        if Char_name.lower() == 'wake' and Char_spell_current.lower() == 'mutilation':
-        if Char_name.lower() == 'finrear' and Char_spell_current.lower() == 'heavy heal':
-        if Char_name.lower() == 'finrear' and Char_spell_current.lower() == 'bolt strike':
-        if Char_name.lower() == 'finrear' and Char_spell_current.lower() == 'buckle armour':
-        if Char_name.lower() == 'chrono' and Char_spell_current.lower() == 'mid heal':
-        if Char_name.lower() == 'chrono' and Char_spell_current.lower() == 'barrel induction':
-        if Char_name.lower() == 'chrono' and Char_spell_current.lower() == 'reinforced pellets':
+        elif Char_name.lower() == 'wake' and Char_spell_current.lower() == 'blood rush':
+        elif Char_name.lower() == 'wake' and Char_spell_current.lower() == 'mutilation':
+        elif Char_name.lower() == 'finrear' and Char_spell_current.lower() == 'heavy heal':
+            if Char_hp_current <= Char_hp_max - 25:
+                Char_hp_current = Char_hp_max
+            else:
+                Char_hp_current = Char_hp_current + 25
+        elif Char_name.lower() == 'finrear' and Char_spell_current.lower() == 'bolt strike':
+        elif Char_name.lower() == 'finrear' and Char_spell_current.lower() == 'buckle armor':
+        elif Char_name.lower() == 'chrono' and Char_spell_current.lower() == 'mid heal':
+            if Char_hp_current <= Char_hp_max - 20:
+                Char_hp_current = Char_hp_max
+            else:
+                Char_hp_current = Char_hp_current + 20
+        elif Char_name.lower() == 'chrono' and Char_spell_current.lower() == 'barrel induction':
+        elif Char_name.lower() == 'chrono' and Char_spell_current.lower() == 'reinforced pellets':
 def Hstl():
     #PLACEHOLDER
