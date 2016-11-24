d046f2f73e105467de4dddcef226bb1012eb0a63
WakeArisato <52734@sunprairieschools.org>
2016-11-23 20:45:48 -0600

    Updated Dialog
    Thats all i really can do sense you've done most of the work with the
    actual coding and i dont know where to pick up.
    Also i really like writing a story to this small demo type game.

--- Help.py
+++ Help.py
@@ -1,2 +1,5 @@
 def Help():
-    print("Help, Check, Attack, Defend, or Wait.")
+    print("You are a Young Hunter with the power to use a Guardian.")
+    print("You are currently locked in a battle with a Vileblood who has the power to use a Dark Beast.")
+    print("Defeat him to win and prevent the summoning of the Harbingers.")
+    print("in battle you have the choice of Help, Check, Attack, Defend, or Wait.")
