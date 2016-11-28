62af342a2c93f9f130ebfb3cb840484e153c5edc
WakeArisato <52734@sunprairieschools.org>
2016-11-28 11:50:20 -0600

    Char Update
    ADDED VIKTOR

--- CharSetup.py
+++ CharSetup.py
@@ -1,7 +1,7 @@
 def CharSelect():
     print("Hello Young Hunter.")
-    print("You have 3 Guaridans to choose from.")
-    Char = input("Wake, Finrear, or Chrono? ")
+    print("You have 4 Guaridans to choose from.")
+    Char = input("Wake, Finrear, Viktor, or Chrono? ")
     global Char_alive, Turn, Char_name, Char_hp_current, Char_etr_max, Char_hp_max, Char_etr_current, Char_def, Char_agl, Char_buff_left, Char_buff_max
     if Char.lower() == 'wake' or Char.lower() == 'w':
         Char_name = 'Wake'
@@ -34,6 +34,18 @@ def CharSelect():
         Char_etr_max = 50
         Char_etr_current = 50
         Char_def = 50
+        Char_agl = 50
+        Char_alive = 1
+        Turn = 0
+        Char_buff_left = 0
+        Char_buff_max = 6
+    elif Char.lower() == 'viktor' or Char.lower() == 'v':
+        Char_name = 'Viktor'
+        Char_hp_current = 75
+        Char_hp_max = 75
+        Char_etr_max = 50
+        Char_etr_current = 50
+        Char_def = 25
         Char_agl = 50
         Char_alive = 1
         Turn = 0
