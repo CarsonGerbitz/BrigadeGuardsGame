1f0f2e96a30e8e08e50bb9a4c34c003d69d8e9fb
WakeArisato <52734@sunprairieschools.org>
2016-12-16 11:36:12 -0600

    dfas
    asdfasdf

--- EVD_Idea.py
+++ EVD_Idea.py
@@ -1,31 +1,33 @@
-def evd(HC,Function):
+def Evade():
+    from Damage import Char_Hit_chance, Hstl_Hit_chance
     import random
     random = random.randint(1,100)
-    if Function == 0:
+    Char_Function = 6
+    if Char_Function == 0:
         Func = .33
-    if Function == 1:
+    if Char_Function == 1:
         Func = .375
-    if Function == 2:
+    if Char_Function == 2:
         Func = .43
-    if Function == 3:
+    if Char_Function == 3:
         Func = .5
-    if Function == 4:
+    if Char_Function == 4:
         Func = .6
-    if Function == 5:
+    if Char_Function == 5:
         Func = .75
-    if Function == 6:
+    if Char_Function == 6:
         Func = 1
-    if Function == 7:
+    if Char_Function == 7:
         Func = 1.33
-    if Function == 8:
+    if Char_Function == 8:
         Func = 1.667
-    if Function == 9:
+    if Char_Function == 9:
         Func = 2
-    if Function == 10:
+    if Char_Function == 10:
         Func = 2.33
-    if Function == 11:
+    if Char_Function == 11:
         Func = 2.667
-    if Function == 12:
+    if Char_Function == 12:
         Func = 3
     HC = (HC*Func)
     if random < HC:
